38210df06d6c8164775897156330eefa7f4b82dd
Alaa Mohammad <alaa85a@gmail.com>
2024-01-10 09:49:56 +0300

    feat: Update the project structure

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Page = () => {
+  return (
+    <>
+      <div>Auth page ...</div>
+    </>
+  );
+};
+export default Page;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Page = () => {
+  return (
+    <>
+      <div>Products page ...</div>
+    </>
+  );
+};
+export default Page;
