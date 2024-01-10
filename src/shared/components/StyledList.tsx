5fe0a8bcca69159a7f4392a814a3874bd7e9ad5f
Alaa Mohammad <alaa85a@gmail.com>
2024-01-10 17:36:44 +0300

    style: Update [StyledList]

--- src/shared/components/StyledList.tsx
+++ src/shared/components/StyledList.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const StyledList = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="[& a]:no-underline [& a]:text-[#fff] mb-[10px] w-[180px] p-[5px]">
+    <div className=" w-full max-w-[360px] [&_a>span]:no-underline [&_a>span]:text-[#fff] mb-[10px] w-[180px] p-[5px]">
       {children}
     </div>
   );
