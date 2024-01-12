81f49c25571489b2b67886415955b789778e9071
Alaa Mohammad <alaa85a@gmail.com>
2024-01-12 12:12:51 +0300

    style: Update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import  Link  from "next/link";
+import Link from "next/link";
 import { ColoredDevider } from "@/shared";
-const NotFound=()=>{
-return (
-    <div
-    className="mx-auto mt-[150px] max-w-[600px] shadow-md hover:shadow-lg text-center py-[10px] text-black"
-      data-testid="NotFound-div"
-    >
-        <div className="text-[16px] text-blue-600">404</div>
-        <div className="text-black uppercase">Page not Found</div>
-        <ColoredDevider/>
-        <Link href='/' className="no-underline text-blue-950">Home Page</Link>
+const NotFound = () => {
+  return (
+    <div className="mx-auto mt-[150px] max-w-[600px] shadow-md hover:shadow-lg text-center py-[10px] text-black">
+      <div className="text-[16px] text-5xl text-blue-600 mb-2">404</div>
+      <div className="text-black uppercase">Page not Found</div>
+      <ColoredDevider />
+      <Link href="/" className="no-underline text-blue-950">
+        Home Page
+      </Link>
     </div>
-)
-}
+  );
+};
 export default NotFound;
