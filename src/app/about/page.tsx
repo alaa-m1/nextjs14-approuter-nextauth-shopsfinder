a7ea3b34a1abe85df4dd39cfd9d343a84a8da18a
Alaa Mohammad <alaa85a@gmail.com>
2024-01-12 12:14:08 +0300

    feat: Update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import { ExternalLink } from "@/shared";
+import React from "react";
+
 const Page = () => {
   return (
-    <>
-      <div>About page ...</div>
-    </>
+    <div
+      className="mx-auto mt-[150px] max-w-[600px] py-[10px] text-center text-black shadow-md hover:shadow-lg"
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          color: "secondary.dark",
+        }}
+      >
+        <span className="font-bold text-[#1976d2]">Alaa Mohammad</span>
+        <span>
+          Senior Frontend Developer | React - JavaScript - TypeScript - Next.js - Test
+          Automation | Software Developer
+        </span>
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <ExternalLink
+          url="https://www.linkedin.com/in/alaa-mohammad-767622199/"
+          title="LinkedIn"
+        />
+        <ExternalLink
+          url="https://www.xing.com/profile/alaa_mohammad8/cv"
+          title="Xing"
+        />
+        <ExternalLink url="https://github.com/alaa-m1" title="GitHub" />
+      </div>
+    </div>
   );
 };
 export default Page;
