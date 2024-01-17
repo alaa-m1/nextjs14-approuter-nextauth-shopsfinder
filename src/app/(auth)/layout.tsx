0d1bf140f6a23d5ec2f3d33fa3c388b8303d8f7a
Alaa Mohammad <alaa85a@gmail.com>
2024-01-17 21:15:31 +0300

    feat: Add signin and signup pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,62 +9,56 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   const pathName = usePathname();
   return (
     <div className="flex h-full">
-      <div className="m-auto w-[500px] h-[300px] shadow-md hover:shadow-lg">
+      <div className="m-auto w-[500px] min-h-[300px] shadow-md hover:shadow-lg">
         <div className="flex flex-col h-full text-black p-3">
           <div className="flex grow">{children}</div>
-          <>
-            <div className="flex justify-center mb-[10px] py-0 px-[10px]">
-              <div className="w-[30%] bg-[#ccc] h-[2px] my-[7px] mx-[5px]" />
-              <span className="w-[30%] text-[#ccc] font-bold">
-                Or continue with
-              </span>
-              <div className="w-[30%] bg-[#ccc] h-[2px] my-[7px] mx-[5px]" />
+
+          <div className="flex justify-center mb-[10px] py-0 px-0.5 w-full">
+            <div className="flex-grow bg-[#ccc] h-[2px] my-[7px] mx-[5px]" />
+            <span className="w-auto text-[#ccc] font-bold">
+              Or continue with
+            </span>
+            <div className=" flex-grow bg-[#ccc] h-[2px] my-[7px] mx-[5px]" />
+          </div>
+          <div className="grid grid-cols-2 justify-center justify-items-center gap-x-0.5 gap-y-2">
+            <div>
+              <button onClick={() => console.log("Sign in using Google")}>
+                <div className="flex flex-col items-center">
+                  <BsGoogle />
+                  Google
+                </div>
+              </button>
             </div>
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 justify-center justify-items-center gap-x-0.5 gap-y-2">
-              <div className="normal-case">
-                <button onClick={() => console.log("Sign in using Google")}>
-                  <div className="flex flex-col">
-                    <BsGoogle className="self-center" />
-                    Google
-                  </div>
-                </button>
-              </div>
-              <div className="normal-case">
-                <button onClick={() => console.log("Sign in using Facebook")}>
-                  <div className="flex flex-col">
-                    <BsFacebook className="self-center" />
-                    <span> Facebook</span>
-                  </div>
-                </button>
-              </div>
+            <div>
+              <button onClick={() => console.log("Sign in using Facebook")}>
+                <div className="flex flex-col items-center">
+                  <BsFacebook />
+                  <span> Facebook</span>
+                </div>
+              </button>
             </div>
-          </>
+          </div>
 
           <ColoredDevider />
           {pathName === "/signup" ? (
-            <div className="w-full flex flex-row justify-between [#_button]:hover:bg-transparent">
-              <div className="grow-[1] size-5 text-black">
+            <div className="w-full flex flex-row justify-left [#_button]:hover:bg-transparent">
+              <div className="w-auto size-5 text-black text-ellipsis overflow-hidden ">
                 If you already have an account &nbsp;
               </div>
-              <LinkButton
-                label="Sign In"
-                style={{ color: "primary.light", fontSize: "15px" }}
-                data-testid="Auth-btn-signin"
-                to="signin"
-              />
+              <LinkButton href="/signin" className="size-[15px]">
+                Sign In
+              </LinkButton>
             </div>
           ) : (
-            <div className="w-full flex flex-row justify-between [#_button]:hover:bg-transparent">
-              <div className="grow-[1] size-5 text-black">
+            <div
+              className="w-full flex flex-row justify-left [#_button]:hover:bg-transparent"
+            >
+              <div className="w-auto size-5 text-black text-ellipsis overflow-hidden ">
                 If you do not have an account &nbsp;
               </div>
-
-              <LinkButton
-                label="Sign Up"
-                style={{ color: "primary.light", fontSize: "15px" }}
-                data-testid="Auth-btn-signup"
-                to="signup"
-              />
+              <LinkButton href="/signup" className="size-[15px]">
+                Sign Up
+              </LinkButton>
             </div>
           )}
         </div>
--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,6 +1,84 @@
-"use client"
+"use client";
 import React from "react";
-const Page=()=>{
-    return <div>Sign In page ... Under construction</div>
-}
+import { MdEmail } from "react-icons/md";
+import { MdLock } from "react-icons/md";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ScaleLoader } from "react-spinners";
+import { LinkButton, LoadingButton, TextField } from "@/shared";
+
+const UserSchema = z.object({
+  email: z.string().email("You must enter a valid Email"),
+  password: z.string(),
+});
+
+type UserSchemaType = z.infer<typeof UserSchema>;
+type SignInProps = {
+  callbackUrl: string;
+  csrfToken: string;
+};
+
+const Page = ({ csrfToken }: SignInProps) => {
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    // reset,
+    formState: { errors, isSubmitting },
+  } = useForm<UserSchemaType>({ resolver: zodResolver(UserSchema) });
+  const onSubmit: SubmitHandler<UserSchemaType> = async () => {
+    console.log("Submit ....");
+  };
+
+  return (
+    <div className="w-full">
+      <h4 className="text-gray-800 font-bold text-center">Sign In</h4>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="my-[5px] mx-[10px] w-full"
+      >
+        <input type="hidden" name="csrfToken" defaultValue={csrfToken} />
+        <TextField
+          name="email"
+          label="Email"
+          placeholder="Email"
+          icon={<MdEmail />}
+          register={register}
+          errors={errors.email?.message}
+          disabled={isSubmitting}
+          autoComplete="off"
+        ></TextField>
+        <TextField
+          name="password"
+          label="Password"
+          placeholder=""
+          icon={<MdLock />}
+          type="password"
+          register={register}
+          errors={errors.password?.message}
+          disabled={isSubmitting}
+          autoComplete="off"
+          defaultValue=""
+        ></TextField>
+        <LoadingButton
+          isLoading={isSubmitting}
+          loadingIndicator={<ScaleLoader color="#36d7b7" />}
+          variant="contained"
+          color="primary"
+          type="submit"
+          className="w-[50%] mx-auto"
+        >
+          Sign In
+        </LoadingButton>
+      </form>
+      <div className="[&_a]:no-underline">
+        <LinkButton href="/forgetpassword" className="size-[15px]">
+          Forget your password
+        </LinkButton>
+      </div>
+    </div>
+  );
+};
+
 export default Page;
