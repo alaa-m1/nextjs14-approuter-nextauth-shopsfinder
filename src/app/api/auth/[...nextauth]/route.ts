42c5bb2565b281f626114fc437e908fb5ee871f8
Alaa Mohammad <alaa85a@gmail.com>
2024-01-21 20:18:10 +0300

    refactor: extract authOptions to utils/nextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-import Auth0Provider from "next-auth/providers/auth0";
+import NextAuth from "next-auth";
+import { authOptions } from "@/utils/nextAuth/authOptions";
 
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID as string,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
-      issuer: process.env.AUTH0_ISSUER as string,
-    }),
-  ],
-  //  Not providing any secret or NEXTAUTH_SECRET will throw an error in production
-  secret: process.env.NEXTAUTH_SECRET,
-};
 const handler = NextAuth(authOptions);
-
-
 export { handler as GET, handler as POST }
