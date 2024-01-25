5d617dbb6f2be66fb4467467463bfb779484e431
Alaa Mohammad <alaa85a@gmail.com>
2024-01-25 14:16:01 +0300

    feat: use SMTP transporter

--- src/utils/mailing/sendEmail.ts
+++ src/utils/mailing/sendEmail.ts
@@ -9,15 +9,25 @@ export default async function sendCustomEmail(
   template: string
 ) {
   const {
-    MAILING_EMAIL,
-    MAILING_PASSWORD,
+    SMTP_PORT,
+    SMTP_HOST,
+    SMTP_EMAIL,
+    SMTP_PASSWORD,
+    // MAILING_EMAIL,
+    // MAILING_PASSWORD,
   } = process.env;
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    port: Number(SMTP_PORT),
+    host: SMTP_HOST,
+    secure:true,
     auth: {
-      user: MAILING_EMAIL,
-      pass: MAILING_PASSWORD,
+      user: SMTP_EMAIL,
+      pass: SMTP_PASSWORD,
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
     },
   });
 
@@ -32,12 +42,14 @@ export default async function sendCustomEmail(
   transporter.verify(function (error) {
     if (error) {
       console.log(error);
-    }
+    }else {
+      console.log('Server is ready to take our messages');
+}
   });
 
   // send the activation email
   const options = {
-    from: MAILING_EMAIL,
+    from: SMTP_EMAIL,
     to,
     subject,
     html,
