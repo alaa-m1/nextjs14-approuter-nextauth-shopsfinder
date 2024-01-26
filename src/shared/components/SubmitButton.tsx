86425f1910a7115bdf93c2200f283da8a8b856ef
Alaa Mohammad <alaa85a@gmail.com>
2024-01-26 14:02:50 +0300

    style: Update [SubmitButton] style when pageLoading and when it is disabled.

--- src/shared/components/SubmitButton.tsx
+++ src/shared/components/SubmitButton.tsx
@@ -22,10 +22,9 @@ export const SubmitButton = ({
   disabled,
   ...props
 }: SubmitButtonProps) => {
-
   const [pageLoading, setPageLoading] = useState(true);
   useEffect(() => setPageLoading(false), []);
-  
+
   const hasBg = useMemo(() => variant === "contained", [variant]);
   return (
     <button
@@ -35,10 +34,16 @@ export const SubmitButton = ({
       type="submit"
       className={classNames(
         "text-white overflow-hidden h-[40px] w-[200px] max-w-[300px] focus:ring-2  focus:ring-blue-500 font-medium rounded-md text-sm px-5 py-1 text-center  transition-all duration-300 inline-flex items-center",
-        { "bg-blue-600": hasBg },
         { "ring-1 ring-blue-800": !hasBg },
-        { "hover:shadow-md": !isLoading && !disabled && !pageLoading},
-        { "hover:bg-blue-800": hasBg && !isLoading && !disabled && !pageLoading}
+        { "hover:shadow-md": !isLoading && !disabled && !pageLoading },
+        {
+          "hover:bg-blue-800 bg-blue-600":
+            hasBg && !isLoading && !disabled && !pageLoading,
+        },
+        {
+          " bg-blue-600": hasBg && isLoading,
+        },
+        { "bg-blue-300": hasBg && (pageLoading || disabled) }
       )}
     >
       <div className="flex w-full justify-center [&_svg]:size-1">
@@ -47,7 +52,9 @@ export const SubmitButton = ({
             {loadingIndicator ?? <RiseLoader size="8" color="#36d7b7" />}
           </div>
         )}
-        <div className="text-gray-800 pl-1 flex place-items-center">{label ? label : "Submit"}</div>
+        <div className="text-gray-800 pl-1 flex place-items-center font-bold">
+          {label || "Submit"}
+        </div>
       </div>
     </button>
   );
