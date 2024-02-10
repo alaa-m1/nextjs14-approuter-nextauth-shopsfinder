08335257062ec8a10157b8378a2592771f54b168
Alaa Mohammad <alaa85a@gmail.com>
2024-02-10 13:41:18 +0300

    feat: Update saveImageToLoacal in uploadProfilePhoto action to use upload-image api route

--- src/app/actions/uploadProfilePhoto.ts
+++ src/app/actions/uploadProfilePhoto.ts
@@ -3,11 +3,11 @@ import jwt from "jsonwebtoken";
 import { DecodedToken } from "@/types";
 import fs from "fs/promises";
 import path from "path";
-import { v4 as uId } from "uuid";
 import os from "os";
 import cloudinary from "cloudinary";
 import { revalidatePath } from "next/cache";
 import ProfilePhoto from "@/utils/mongoLib/models/ProfilePhoto";
+import { getFileName } from "@/utils/uploader/getFileName";
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -16,20 +16,27 @@ cloudinary.v2.config({
 });
 
 async function saveImageToLoacal(formData: FormData) {
+  const uploadFormData = new FormData();
   const file = formData.get("profilePhoto") as File;
-  const fileBufferPromise = file.arrayBuffer().then((data) => {
-    const buffer = Buffer.from(data);
-    const uniqueName = `${uId()}--${file.name}`;
-    const ext = file.type.split("/")[1];
-
-    const tempDir = os.tmpdir();
-    const uploadDir = path.join(tempDir, `/${uniqueName}.${ext}`);
-
-    fs.writeFile(uploadDir, buffer);
-    return { filePath: uploadDir, fileName: uniqueName };
+  const { uniqueName, fullName } = getFileName(file);
+  uploadFormData.append("fullName", fullName);
+  uploadFormData.append("profilePhoto", file);
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/upload-image`, {
+    method: "POST",
+    body: uploadFormData,
   });
 
-  return await fileBufferPromise;
+  const data = await response.json();
+  let status: "200" | "500" = "200";
+
+  if (data.msg !== "uploaded success") {
+    status = "500";
+  }
+
+  const tempDir = os.tmpdir();
+  const uploadDir = path.join(tempDir, `/${fullName}`);
+
+  return { filePath: uploadDir, fileName: uniqueName, status };
 }
 
 async function uploadImgaesToCloudinary(newFile: {
@@ -45,6 +52,14 @@ async function uploadImgaesToCloudinary(newFile: {
 export async function uploadProfilePhoto(formData: FormData) {
   try {
     const newFile = await saveImageToLoacal(formData);
+    if (newFile.status === "500") {
+      return {
+        message: "File upload failed, please try later",
+        status: 500,
+        imageUrl: "",
+        publicId: "",
+      };
+    }
     const id = formData.get("userId") as string;
     const imagePublicId = formData.get("imagePublicId") as string;
 
