d827b32de80e5bff715f89d0c24c28521b9ab882
Alaa Mohammad <alaa85a@gmail.com>
2024-03-05 21:12:21 +0300

    refactor: Update [ProductsListLoadingSkeleton]

--- src/app/products/components/ProductsListLoadingSkeleton.tsx
+++ src/app/products/components/ProductsListLoadingSkeleton.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import _ from "lodash";
 const GRID_ITEMS_COUNT = 10;
 export const ProductsListLoadingSkeleton = () => {
-  const gridItemsArray = Array(GRID_ITEMS_COUNT)
-    .fill(0)
-    .map((item) => ({ label: item, id: _.uniqueId() }));
+  const gridItemsArray = useMemo(
+    () =>
+      Array(GRID_ITEMS_COUNT)
+        .fill(0)
+        .map((item) => ({ label: item, id: _.uniqueId() })),
+    []
+  );
   return (
     <div>
       <div className="grid gap-5 justify-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-[10px]">
         {gridItemsArray.map((item) => (
           <div key={item.id} className="w-[300px] h-[350px]">
-            <Skeleton style={{ width: "100%", height: "100%" }} />
+            <Skeleton style={{ width: "100%", height: "100%" }}/>
           </div>
         ))}
       </div>
