7d30bc8901b38d5fa3b74a5c2dac5cadf178ca9b
Alaa Mohammad <alaa85a@gmail.com>
2024-09-14 13:22:36 +0300

    refactor: get session using getSession method

--- src/shared/appContainers/AppRootProvider.tsx
+++ src/shared/appContainers/AppRootProvider.tsx
@@ -9,7 +9,7 @@ import { SessionProvider } from "next-auth/react";
 
 type NextAuthProvider = {
   children: React.ReactNode;
-  session: Session;
+  session: Session | null;
 };
 
 const RootProvider = ({ children, session }: NextAuthProvider) => {
