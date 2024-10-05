c22ac7b63f51c4679d33bab44b88fa5b8041097a
Alaa Mohammad <alaa85a@gmail.com>
2024-10-05 10:33:47 +0300

    fix: Update edit profile link href to support languages

--- src/app/[lang]/dashboard/page.tsx
+++ src/app/[lang]/dashboard/page.tsx
@@ -3,18 +3,19 @@ import { UserCard, UserProfile, UserSettings } from "./components";
 // import { PhotosPage } from "./components/CloudinaryPhotosList/PhotoPage";
 type DashboardProps = {
   searchParams: { p: string };
+  params: { lang: string };
 };
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
-const Page = ({ searchParams }: DashboardProps) => {
+const Page = ({ searchParams, params }: DashboardProps) => {
   return (
     <>
-    {/* <PhotosPage/> */}
-    <br />
-    <br />
-    <br />
-      {!searchParams.p && <UserCard />}
+      {/* <PhotosPage/> */}
+      <br />
+      <br />
+      <br />
+      {!searchParams.p && <UserCard lang={params.lang} />}
       {searchParams.p?.toLocaleLowerCase() === "profile" && <UserProfile />}
       {searchParams.p?.toLocaleLowerCase() === "settings" && <UserSettings />}
     </>
