2d256bcb1c0638753929f2a9c81cdf21aaf55e00
Alaa Mohammad <alaa85a@gmail.com>
2024-12-05 18:32:07 +0300

    Feat: Update languageMenu navigation functionality to support the using of route groups.

--- src/shared/layoutComponents/Header/components/LanguageMenu.tsx
+++ src/shared/layoutComponents/Header/components/LanguageMenu.tsx
@@ -8,7 +8,6 @@ import {
   DropdownItem,
   Button,
 } from "@nextui-org/react";
-// import { ImEarth } from "react-icons/im";
 import { RiGlobalLine } from "react-icons/ri";
 import { languages } from "@/app/i18n/settings";
 import Link from "next/link";
@@ -22,21 +21,27 @@ export const LanguageMenu = ({ lang }: { lang: string }) => {
   const segments = useSelectedLayoutSegments();
   const { t } = useTranslation(lang);
   const { updateLanguage } = useLanguage();
+
   useEffect(() => {
     if (lang === "ar" || lang === "en" || lang === "de") {
       updateLanguage(lang);
     }
   }, [lang, updateLanguage]);
 
-  const currentSegments = (segments ?? []).join("/");
+  // Filter out route groups (segments starting with "(" )
+  const currentSegments = (segments ?? [])
+    .filter((segment) => !segment.startsWith("("))
+    .join("/");
+
   const isClient = useIsClient();
+
   return (
     <Dropdown>
       <DropdownTrigger>
         <Button
           isIconOnly
           className={classNames(
-            "px-2 border-none bg-transparent  hover:[&>svg]:text-[#e76712] hover:[&>svg]:transition-all hover:[&>svg]:duration-300",
+            "px-2 border-none bg-transparent hover:[&>svg]:text-[#e76712] hover:[&>svg]:transition-all hover:[&>svg]:duration-300",
             { "[&>svg]:text-white": !isClient },
             {
               "[&>svg]:text-light-label [&>svg]:dark:text-dark-label": isClient,
@@ -50,24 +55,22 @@ export const LanguageMenu = ({ lang }: { lang: string }) => {
         aria-label="Static Actions"
         className="p-[10px] rounded-sm bg-light-card dark:bg-dark-card"
       >
-        {languages.map((l) => {
-          return (
-            <DropdownItem
-              key={l}
-              className="text-light-primary dark:text-dark-primary p-0"
-              color="default"
+        {languages.map((l) => (
+          <DropdownItem
+            key={l}
+            className="text-light-primary dark:text-dark-primary p-0"
+            color="default"
+          >
+            <Link
+              href={`/${l}/${currentSegments}`}
+              className="block w-full h-full p-1"
             >
-              <Link
-                href={`/${l}/${currentSegments}`}
-                className="block w-full h-full p-1"
-              >
-                {l === "ar" ? t("arabic") : ""}
-                {l === "en" ? t("english") : ""}
-                {l === "de" ? t("german") : ""}
-              </Link>
-            </DropdownItem>
-          );
-        })}
+              {l === "ar" ? t("arabic") : ""}
+              {l === "en" ? t("english") : ""}
+              {l === "de" ? t("german") : ""}
+            </Link>
+          </DropdownItem>
+        ))}
       </DropdownMenu>
     </Dropdown>
   );
